ada89602f40b43cc5252598c6a48bd993a09bada
liej6799 <liej6799@users.noreply.github.com>
2026-09-24 14:46:37 +0000

    mb/lenovo/x270: Default to ACPI S3 sleep

--- src/mainboard/lenovo/sklkbl_thinkpad/cfr.c
+++ src/mainboard/lenovo/sklkbl_thinkpad/cfr.c
@@ -74,7 +74,17 @@ static struct sm_obj_form *sm_root[] = {
 	NULL
 };
 
+#if CONFIG(BOARD_LENOVO_X270)
+static const struct cfr_default_override x270_cfr_overrides[] = {
+	CFR_OVERRIDE_BOOL("s0ix_enable", false),
+	CFR_OVERRIDE_END
+};
+#endif
+
 void mb_cfr_setup_menu(struct lb_cfr *cfr_root)
 {
+#if CONFIG(BOARD_LENOVO_X270)
+	cfr_register_overrides(x270_cfr_overrides);
+#endif
 	cfr_write_setup_menu(cfr_root, sm_root);
 }
